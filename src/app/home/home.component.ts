87d9fdf0ff971911298509f2d398b9737faa8c56
José Daniel Calle Briceño <j.daniel.c137@gmail.com>
2022-09-07 19:01:07 -0500

    a

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { FormBuilder } from 'ire-forms';
 
 enum Operation {
-  Sum = 'sum',
+  Rolliza = 'Rolliza Doyle',
+  Aserrada = 'Aserrada',
 }
 
 interface Item {
@@ -17,30 +18,69 @@ interface Item {
 })
 export class HomeComponent {
   list: Item[] = [];
+  operationsList = Operation;
+  currentOperation!: Operation;
 
-  readonly form = FormBuilder.build({
+  readonly options = FormBuilder.build({
     fields: [
       FormBuilder.select({
         name: 'op',
         label: 'Operation',
         options: [
           {
-            label: 'Sum',
-            value: Operation.Sum,
+            label: Operation.Rolliza,
+            value: Operation.Rolliza,
+          },
+          {
+            label: Operation.Aserrada,
+            value: Operation.Aserrada,
           },
         ],
         index: 0,
       }),
+    ],
+  });
+
+  readonly op1 = FormBuilder.build({
+    legend: 'Rolliza Doyle',
+    // styles: css``,
+    fields: [
       FormBuilder.numberInput({
-        name: 'arg1',
-        label: 'Argument 1',
+        name: 'dap',
+        label: 'DAP',
       }),
       FormBuilder.numberInput({
-        name: 'arg2',
-        label: 'Argument 2',
+        name: 'lon',
+        label: 'LON',
       }),
     ],
   });
+
+  readonly op2 = FormBuilder.build({
+    legend: 'Aserrada',
+    fields: [
+      FormBuilder.numberInput({
+        name: 'e',
+        label: 'E',
+      }),
+      FormBuilder.numberInput({
+        name: 'a',
+        label: 'A',
+      }),
+      FormBuilder.numberInput({
+        name: 'l',
+        label: 'L',
+      }),
+    ],
+  });
+
+  ngOnInit() {
+    // this.currentOperation = this.options.value['op'];
+    this.options.valueChanges.subscribe(({ op }) => {
+      this.reset();
+      this.currentOperation = op;
+    });
+  }
 
   ngAfterViewInit() {
     // this.form.valueChanges.subscribe((value) => {
@@ -48,14 +88,32 @@ export class HomeComponent {
     // });
   }
 
-  handleSubmit() {
-    const { op, arg1, arg2 } = this.form.value;
+  async handleSubmit() {
+    let value = 0;
+    switch (this.currentOperation) {
+      case Operation.Rolliza:
+        const { dap, lon } = this.op1.value;
+        value = (Math.pow(dap - 4, 2) / 16) * lon;
+        break;
+      case Operation.Aserrada:
+        const { e, a, l } = this.op2.value;
+        value = (e * a * l) / 12;
+        break;
+    }
     this.list.push({
-      // value: Number.parseFloat(arg1) + Number.parseFloat(arg2),
-      operation: op,
-      value: arg1 + arg2,
+      operation: this.currentOperation,
+      value,
     });
-    this.form.reset();
-    this.form.get('arg1')?.focus();
+    this.reset();
+    this.options.get('arg1')?.focus();
+  }
+
+  reset() {
+    // this.options.reset();
+    // this.op1.reset();
+    // this.op2.reset();
+    // this.options.clean();
+    // this.op1.clean();
+    // this.op2.clean();
   }
 }
